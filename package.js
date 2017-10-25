23835bc89561d2ebcb084610ccc97fcd4434ebec
defc0n1 <defc0n1@users.noreply.github.com>
2017-10-25 09:33:08 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'defc0n1:accounts-invite',
-  version: '1.2.2',
+  version: '1.2.3',
   summary: 'Account validation with oauth and anonymous user support',
   git: 'https://github.com/t3db0t/meteor-accounts-invite',
   documentation: 'README.md'
