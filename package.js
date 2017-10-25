e3b2efac0705bba3a8596d83b84c8858b6901169
defc0n1 <defc0n1@users.noreply.github.com>
2017-10-25 09:32:49 +0200

    Update package.js

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.export('AccountsInvite');
   
   api.use(['accounts-base'], 'server');
-  api.use(['check', 't3db0t:accounts-multiple@0.3.2', 'brettle:accounts-patch-ui@0.1.12', 'brettle:accounts-add-service']);
+  api.use(['check', 't3db0t:accounts-multiple@0.3.2', 'brettle:accounts-patch-ui', 'brettle:accounts-add-service']);
   // brettle:accounts-patch-ui
 
   // Allow us to call Accounts.oauth.serviceNames, if there are any OAuth services.
